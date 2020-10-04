b5653b7193839892d933a7cac47a303011ad59f5
Diego C <42076222+cmdiego@users.noreply.github.com>
2020-10-04 18:03:07 -0400

    Update SignUp.php

--- LAMPAPI/SignUp.php
+++ LAMPAPI/SignUp.php
@@ -32,17 +32,49 @@ else
     if ($result)
     {
         echo "User . '$login' was successfully added to the database";
+
+            // New SQL query string to return ID of user to front-end
+        $sql = "SELECT ID FROM Users WHERE login = '$login'";
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) 
+        {
+            $row = $result->fetch_assoc();
+            $id = $row["ID"];
+            // Sucessful search
+            echo "Login: " . $login " ID: " . $id
+
+            returnWithInfo($id);
+        }
     }
     else
     {
         echo "Error: " . $sql . "" . mysql_error($conn);
     }
-    $conn->close();
 }
 
 function getRequestInfo()
 {
     return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithInfo( $searchResults )
+{
+    $retValue = '{"results":[' . $searchResults . ']}';
+    sendResultInfoAsJson( $retValue );
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
 }
 
 ?>
